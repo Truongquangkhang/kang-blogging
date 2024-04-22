bd511838520a5448b50425c4b984355b4a856877
Truongquangkhang <truongqkhang@gmail.com>
2024-04-22 22:39:41 +0700

    Test github actions

---
+++ scripts/dockerize.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+readonly image_tag="$1"
+readonly image_repository="$2"
+readonly type="$3"
+readonly service="$4"
+readonly push_image="$5"
+
+buildDir="$type"
+if [ "$type" == "app" ]; then
+  buildDir="internal"
+fi
+
+docker build \
+  --no-cache \
+  --progress=plain \
+  --tag "$image_repository":"$image_tag" \
+  --file "./docker/$type/Dockerfile" \
+  --build-arg "SERVICE=$service" \
+  "./$buildDir"
+
+if [ "$push_image" == "true" ]; then
+  docker push "$image_repository":"$image_tag"
+fi
