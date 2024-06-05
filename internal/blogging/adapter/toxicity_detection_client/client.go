f5ad4e3d846716e2bff5b6f28491849fb76a6cf2
Truongquangkhang <truongqkhang@gmail.com>
2024-06-05 19:02:58 +0700

    Update

--- internal/blogging/adapter/toxicity_detection_client/client.go
+++ internal/blogging/adapter/toxicity_detection_client/client.go
@@ -1,15 +1,10 @@
 package toxicity_detection_client
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"golang.org/x/net/context"
-	"io"
 	"kang-blogging/internal/blogging/domain/detection_client"
-	"kang-blogging/internal/common/server/httpheader"
-	"net/http"
 	"os"
+	"strings"
 )
 
 type ToxicityDetectionClient struct {
@@ -32,60 +27,61 @@ const (
 	DETECT_CONTENT = "/api/v1/detect-content/"
 )
 
-//func (c *ToxicityDetectionClient) DetectToxicComment(
-//	ctx context.Context,
-//	comment string,
-//) (*detection_client.PredictionComment, error) {
-//	countLetter := strings.Count(comment, " ") + 1
-//	prediction := make([]int32, countLetter)
-//	isToxicComment := false
-//	for index, c := range strings.Split(comment, " ") {
-//		if c == "ngu" {
-//			isToxicComment = true
-//			prediction[index] = 1
-//		}
-//	}
-//	return &detection_client.PredictionComment{
-//		IsToxicComment:         isToxicComment,
-//		Comment:                comment,
-//		ToxicPredictionComment: prediction,
-//	}, nil
-//}
-
 func (c *ToxicityDetectionClient) DetectToxicComment(
 	ctx context.Context,
 	comment string,
 ) (*detection_client.PredictionComment, error) {
-	body := []byte(fmt.Sprintf(`{
-		"content": "%s"
-	}`, comment))
-	response, err := http.Post(
-		fmt.Sprintf("%s%s", c.URL, DETECT_CONTENT),
-		httpheader.CONTENT_TYPE_APPLICATION_JSON, bytes.NewBuffer(body),
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	responseData, err := io.ReadAll(response.Body)
-	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %v", err)
-	}
-	var detectContentResponse DetectContentResponse
-	err = json.Unmarshal(responseData, &detectContentResponse)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling response body: %v", err)
-	}
+	countLetter := strings.Count(comment, " ") + 1
+	prediction := make([]int32, countLetter)
 	isToxicComment := false
-	for _, c := range detectContentResponse.Predictions {
-		if c == 1 {
+	for index, c := range strings.Split(comment, " ") {
+		if c == "ngu" {
 			isToxicComment = true
-			break
+			prediction[index] = 1
 		}
 	}
 	return &detection_client.PredictionComment{
 		IsToxicComment:         isToxicComment,
-		Comment:                detectContentResponse.Text,
-		ToxicPredictionComment: detectContentResponse.Predictions,
+		Comment:                comment,
+		ToxicPredictionComment: prediction,
 	}, nil
 }
+
+//
+//func (c *ToxicityDetectionClient) DetectToxicComment(
+//	ctx context.Context,
+//	comment string,
+//) (*detection_client.PredictionComment, error) {
+//	body := []byte(fmt.Sprintf(`{
+//		"content": "%s"
+//	}`, comment))
+//	response, err := http.Post(
+//		fmt.Sprintf("%s%s", c.URL, DETECT_CONTENT),
+//		httpheader.CONTENT_TYPE_APPLICATION_JSON, bytes.NewBuffer(body),
+//	)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	responseData, err := io.ReadAll(response.Body)
+//	if err != nil {
+//		return nil, fmt.Errorf("error reading response body: %v", err)
+//	}
+//	var detectContentResponse DetectContentResponse
+//	err = json.Unmarshal(responseData, &detectContentResponse)
+//	if err != nil {
+//		return nil, fmt.Errorf("error unmarshalling response body: %v", err)
+//	}
+//	isToxicComment := false
+//	for _, c := range detectContentResponse.Predictions {
+//		if c == 1 {
+//			isToxicComment = true
+//			break
+//		}
+//	}
+//	return &detection_client.PredictionComment{
+//		IsToxicComment:         isToxicComment,
+//		Comment:                detectContentResponse.Text,
+//		ToxicPredictionComment: detectContentResponse.Predictions,
+//	}, nil
+//}
